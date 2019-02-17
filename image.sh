673ff0a5e78f77b23154ce5815b9693c16ee945d
Graham Wihlidal <graham@wihlidal.ca>
2019-02-17 19:41:37 +0100

    Updated docker base image version

--- image.sh
+++ image.sh
@@ -1,3 +1,3 @@
-docker pull gwihlidal/docker-shader:vk_rt
-docker build -t gwihlidal/svc-shader:2 .
-docker push gwihlidal/svc-shader:2
+docker pull gwihlidal/docker-shader:vk_rt3
+docker build -t gwihlidal/svc-shader:5 .
+docker push gwihlidal/svc-shader:5
