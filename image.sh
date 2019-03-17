65ed132a8eb94a626564f62dde00b2bc4ed49686
Graham Wihlidal <graham@wihlidal.ca>
2019-03-17 16:16:17 -0700

    Updated to v8

--- image.sh
+++ image.sh
@@ -1,3 +1,3 @@
-docker pull gwihlidal/docker-shader:5
-docker build -t gwihlidal/svc-shader:7 .
-docker push gwihlidal/svc-shader:7
+docker pull gwihlidal/docker-shader:6
+docker build -t gwihlidal/svc-shader:8 .
+docker push gwihlidal/svc-shader:8
